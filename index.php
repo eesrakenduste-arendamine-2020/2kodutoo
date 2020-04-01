8258289a0fba0636332dd5fb8c1d0cdf0dd684a9
Liibro <liina@22systems.ee>
2020-04-01 19:21:53 +0300

    Search box styles

--- index.php
+++ index.php
@@ -15,12 +15,33 @@
 
 <div class="container">
 
-    <div class="item i1"><div class="i1__box"></div></div>
-    <div class="item i2"><div class="i2__box"></div></div>
-    <div class="item i3"><div class="i3__box"></div></div>
-    <div class="item i4"><div class="i4__box"></div></div>
+    <div class="item i1">
+        <div class="i1__box">
 
-</div>
+        </div>
+    </div>
 
+    <div class="item i2">
+        <div class="i2__box">
+
+        </div>
+    </div>
+
+    <div class="item i3">
+        <div class="i3__box">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="36px" height="36px"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                <input class="input-field" type="text">
+        </div>
+    </div>
+
+    <div class="item i4">
+        <div class="i4__box">
+
+        </div>
+    </div>
+
+</div><!--.container-->
+
+<script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+    $(document).on('focusin', '.input-field', function() {
+        $('.icon').css('opacity', 0);
+    });
+
+    $(document).on('focusout', '.input-field', function() {
+        if ($(this).val() == '') {
+            $('.icon').css('opacity', 1);
+        }
+    });
+
 });
