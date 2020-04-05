05100eae0060d75867379255aad60ccc6331c424
Daniel Eelmaa <daneelm@users.noreply.github.com>
2020-04-05 17:47:19 +0300

    Daniel entry 2

--- todo.js
+++ todo.js
@@ -14,6 +14,8 @@ function init() {
   getTodoData();
 }
 
+
+
 function getTodoData() {
   let request = new XMLHttpRequest();
   request.open("GET", "todo.txt");
@@ -29,6 +31,8 @@ function getTodoData() {
   };
   request.send();
 }
+
+
 
 function parseTodoItems(todoJSON) {
   if (todoJSON == null || todoJSON.trim() == "") {
@@ -46,6 +50,50 @@ function parseTodoItems(todoJSON) {
 }
   
 
+
+function addTodosToPage() {
+  let ul = document.getElementById("todoList");
+  document.forms[0].reset();  
+  for (let i = 0; i < todos.length; i++) {
+    let todoItem = todos[i];
+    let li = document.createElement("li");
+    if (todoItem.deleted == null) {
+      
+      li.innerHTML = todoItem.title + "  " + todoItem.description + " by " + todoItem.dueDate;
+      const removeButton = document.createElement("div");
+      removeButton.className = "delete-button";
+      const removeIcon = document.createTextNode("X");
+      removeButton.addEventListener("click", () => {
+        ul.removeChild(li);
+        todoItem.deleted = "yes";
+        saveTodoData();
+      });
+      removeButton.appendChild(removeIcon);
+      li.appendChild(removeButton);
+      ul.appendChild(li);
+    }
+  }
+}
+
+
+function getFormData() {
+  let description = document.getElementById("description").value;
+  if (checkInputText(description, "Please enter a description")) return;
+
+  let title = document.getElementById("title").value;
+  if (checkInputText(title, "Please enter a title")) return;
+
+  let date = document.getElementById("dueDate").value;
+  if (checkInputText(date, "Please enter a due date")) return;
+
+  console.log("New description " + description + ", for: " + title + ", by: " + date);
+  let todoItem = new Todo(description, title, date);
+  todos.push(todoItem);
+  saveTodoData();
+  document.getElementById("todoList").innerHTML = "";
+
+
+}
 
 
 function saveTodoData() {
