5f6ba9bd43f1dd6f30e23e545533819a467dae6c
Henry Pajuri <55189736+HenryPajuri@users.noreply.github.com>
2020-04-05 20:10:28 +0300

    Add files via upload
    edit ikka ei tööta..

--- Todo app/todo.js
+++ Todo app/todo.js
@@ -1,6 +1,6 @@
-/*window.onload = function(){
-  document.write(count);
-}*/
+window.onload = function() {
+  editPage.style.display = "none";
+};
 
 class Entry {
   constructor(title, description, date) {
@@ -8,113 +8,70 @@ class Entry {
     this.description = description;
     this.date = date;
     this.done = false;
-   
   }
 }
 
-let entries = [];
-
 let count = 0;
-let countContainer = document.querySelector('#count');
+let countContainer = document.querySelector("#count");
 
 const clear = document.querySelector(".clear");
 
 clear.addEventListener("click", function() {
   localStorage.clear();
   location.reload();
-  title.value='';
-  description.value="";
+  title.value = "";
+  description.value = "";
 });
-
-
 
 class Todo {
   constructor() {
+    //$('#entries').on('click', '#editLink', setCurrent)
+    //$("#submitEdit").on("click", editTask);
+    //$("#submitEdit").on("click", setCurrent);
     this.entries = JSON.parse(window.localStorage.getItem("entries")) || [];
-    $('#submitEdit').on('click', editTask);
     document.querySelector("#save").addEventListener("click", () => {
       this.addEntry();
       this.saveToFile();
-      
+    });
+    document.querySelector("#submitEdit").addEventListener("click", () => {
+      this.editTask();
+      this.saveLocal();
+      this.showTodos();
     });
 
-    
-   
     this.render();
   }
-    
-  saveToFile(){
-  
-    $.post('server.php', {save: this.entries}).done(function(){
-         alert('Success');
-     }).fail(function(){
-         alert('FAIL');
-     });
-   
+
+  saveToFile() {
+    $.post("server.php", { save: this.entries })
+      .done(function() {
+        //alert("Success");
+      })
+      .fail(function() {
+        alert("FAIL");
+      });
   }
 
-  setCurrent(){
-    localStorage.setItem('currentTitle', $(this).data('title'));
-    localStorage.setItem('currentDescription', $(this).data('description'));
-    localStorage.setItem('currentDate', $(this).data('date'));
-
-    $('#editTitle').val(localStorage.getItem('currentTitle'));
-    $('#editDate').val(localStorage.getItem('currentDescription'));
-    $('#editDescription').val(localStorage.getItem('currentDate'));
-  }
-
-  editTask(){
-      let currentTitle = localStorage.getItem('currentTitle');
-      let currentDate = localStorage.getItem('currentDate');
-      let currentDescription = localStorage.getItem('currentDescription');
-  
-      this.entries = getEntryObject(); 
-  
-      for(let i=0; i< this.entries.length; i++){
-        if(this.entries[i].Title == currenttitle && this.entries[i].date == currentDate && this.entries[i].description == currentDescription){
-          this.entries.splice(i, 1);
-        }
-        localStorage.setItem('entries', JSON.stringify(this.entries));
-      }
-  
-      let titleValue = $('#editTitle').val();
-      let dateValue = $('#editDate').val();
-  
-      let update_todo = {
-        titleValue: titleValue,
-        date: dateValue,
-        descriptionValue: descriptionValue 
-      };
-  
-      this.entries.push(update_todo);
-  
-      alert("Ülesanne muudetud");
-  
-      localStorage.setItem('entries', JSON.stringify(this.entries));
-  
-      window.location.href = "todo.html";
-  
-      return false;
-    }
-
-  
-  
   addEntry() {
+    const title = $("#title").val();
+    const description = $("#description").val();
+    const date = $("#date").val();
 
-    const titleValue = $('#title').val();
-    const descriptionValue = $('#description').val();
-    const dateValue = $('#date').val();
+    let todo = {
+      title: title,
+      description: description,
+      date: date
+    };
+    this.entries.push(todo);
+    count++;
 
-    this.entries.push(new Entry(titleValue, descriptionValue, dateValue));
-    count ++;
-   
-    countContainer.innerHTML = count;
     console.log(this.entries);
     this.saveLocal();
     this.render();
   }
 
   render() {
+    countContainer.innerHTML = "You have " + count + " task(s) left to do.";
     if (document.querySelector(".todo-list")) {
       document.body.removeChild(document.querySelector(".todo-list"));
     }
@@ -129,18 +86,31 @@ class Todo {
       const removeButton = document.createElement("div");
       removeButton.className = "delete-button";
       const removeIcon = document.createTextNode("X");
+      const editButton = document.createElement("div");
+      editButton.className = "edit-button";
+      const editIcon = document.createTextNode("EDIT");
 
       div.innerHTML = `<div>${entryValue.title}</div><div> ${entryValue.description}</div>
             <div>${entryValue.date}</div>`;
 
       removeButton.addEventListener("click", () => {
+        count = count - 1;
         ul.removeChild(li);
         this.entries.splice(entryIndex, 1);
         this.saveLocal();
         this.render();
-        this.setCurrent(); 
+
+        //this.setCurrent();
         //this.showTodos();
-        
+      });
+
+      editButton.addEventListener("click", () => {
+        //editButton.addEventListener('click', () => this.edit(input, name));
+        editPage.style.display = "block";
+        //this.entries.splice(entryIndex, 1);
+        // this.setCurrent();
+        //this.editTask();
+        this.render();
       });
 
       if (entryValue.done) {
@@ -153,7 +123,6 @@ class Todo {
           this.entries[entryIndex].done = false;
           this.saveLocal();
           this.saveToFile();
-
         } else {
           li.classList.add("task-completed");
           this.entries[entryIndex].done = true;
@@ -161,6 +130,10 @@ class Todo {
           this.saveToFile();
         }
       });
+      editButton.appendChild(editIcon);
+      li.appendChild(div);
+      li.appendChild(editButton);
+      ul.appendChild(li);
 
       removeButton.appendChild(removeIcon);
       li.appendChild(div);
@@ -171,41 +144,103 @@ class Todo {
     document.body.appendChild(ul);
   }
 
+  /*edit(input, entryIndex){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = entries.indexOf(entryIndex);
+            entries[indexof] = input.value;
+            this.saveLocal();
+        }
+    }*/
+
   saveLocal() {
     window.localStorage.setItem("entries", JSON.stringify(this.entries));
   }
 
+  showTodos() {
+    this.entries = getEntryObject();
 
-  showTodos(){
-    this.entries = getTodoObject();
-
-    if(this.entries != "" && this.entries != null){
-      for(let i = 0; i < this.entries.length; i++){
-        $('#this.entries').append('<li class="ui-body-inherit ui-li-static">'+ this.entries[i].task+ '<br>'+ this.entries[i].date +'<div class="controls"><a href="#edit" id="editLink" data-task="'+this.entries[i].task + '" data-date="' + this.entries[i].date + '">Muuda</a> | <a href="#" id="deleteLink" data-task="'+this.entries[i].task + '" data-date="' + this.entries[i].date + '" onclick="return confirm(\'Kas oled kindel, et tahad ülesannet kustutada?\')">Kustuta</a></div></li>');
+    if (this.entries != "" && this.entries != null) {
+      for (let entryIndex = 0; entryIndex < this.entries.length; entryIndex++) {
+        $("#this.entries").append(
+          '<li class="ui-body-inherit ui-li-static">' +
+            this.entries[entryIndex].title +
+            "<br>" +
+            this.entries[entryIndex].description +
+            "<br>" +
+            this.entries[entryIndex].date +
+            '<div class="controls"><a href="#editPage" id="editLink" data-task="'
+        );
       }
     }
+  }
+  setCurrent() {
+    localStorage.setItem("currentTitle", $(this).data("title"));
+    localStorage.setItem("currentDescription", $(this).data("description"));
+    localStorage.setItem("currentDate", $(this).data("date"));
 
+    $("#editTitle").val(localStorage.getItem("currentTitle"));
+    $("#editDescription").val(localStorage.getItem("currentDescription"));
+    $("#editDate").val(localStorage.getItem("currentDate"));
   }
 
+  editTask() {
+    let currentTitle = localStorage.getItem("currentTitle");
+    let currentDescription = localStorage.getItem("currentDescription");
+    let currentDate = localStorage.getItem("currentDate");
 
+    entries = getEntryObject();
 
-  getEntryObject(){
-    let currentEntries = localStorage.getItem('entries');
+    for (let entryIndex = 0; entryIndex < this.entries.length; entryIndex++) {
+      if (
+        this.entries[entryIndex].title == currentTitle &&
+        this.entries[entryIndex].date == currentDate &&
+        this.entries[entryIndex].description == currentDescription
+      ) {
+        this.entries.splice(entryIndex, 1);
+      }
+      localStorage.setItem("entries", JSON.stringify(this.entries));
+    }
 
-    if(currentEntries != null){
+    let title = $("#editTitle").val();
+    let date = $("#editDate").val();
+    let description = $("#editDescription").val();
+
+    let update_todo = {
+      title: title,
+      description: description,
+      date: date
+    };
+
+    this.entries.push(update_todo);
+
+    this.saveLocal();
+    this.render();
+    alert("Ülesanne muudetud");
+
+    localStorage.setItem("entries", JSON.stringify(this.entries));
+
+    window.location.href = "todo.html";
+
+    return false;
+  }
+
+  getEntryObject() {
+    let currentEntries = localStorage.getItem("entries");
+
+    if (currentEntries != null) {
       this.entries = JSON.parse(currentEntries);
-    } else{
+    } else {
       this.entries = [];
     }
 
-    return this.entries.sort(function(a, b){
+    return this.entries.sort(function(a, b) {
       return new Date(b.date) - new Date(a.date);
     });
-
   }
-
- 
-
 }
 
 const todo = new Todo();
