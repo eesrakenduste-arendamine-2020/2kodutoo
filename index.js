e4f5eb08b56287d83d91ae3b002010334a349b80
Daniel Gurevits <43746225+mitsukiz@users.noreply.github.com>
2020-04-05 03:31:38 +0300

    Update index.js
    Lisasin sorteerimise ja otsinguriba funktsionaalsused ning parandasin mõned pugid, mille pärast tekkisid lehel vead.

--- index.js
+++ index.js
@@ -61,6 +61,10 @@ class Todo{
 
         document.querySelector('#addButton').addEventListener('click', ()=>{this.addEntry()});
 
+        document.querySelector("#sort").addEventListener("change", ()=>{this.sortFunction();});
+
+        document.querySelector(".searchform").addEventListener("keyup", () =>{this.searchFunction();});
+
         this.render();
     }
 
@@ -80,6 +84,38 @@ class Todo{
 
     }
 
+    sortFunction() {
+        const sortbutton = document.getElementById("sort");
+        var selected = sortbutton.value;
+        console.log(selected);
+        if (selected == "2") {
+          this.entries.sort((a, b) => a.title.localeCompare(b.title));
+          this.render();
+        } else if (selected == "1") {
+          this.entries.sort((a, b) => a.date.localeCompare(b.date));
+          this.render();
+        }
+      }
+
+      searchFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("searchInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("todo");
+        li = ul.getElementsByTagName("li");
+        console.log(li);
+
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }
+
     render(){
         if(document.querySelector('.todo-list')){
             document.body.removeChild(document.querySelector('.todo-list'));
@@ -87,12 +123,13 @@ class Todo{
         }
         const ul = document.createElement('ul');
         ul.className = 'todo-list';
+        ul.id = "todo";
         this.entries.forEach((entryValue, entryIndex)=>{
             const li = document.createElement('li');
             const div = document.createElement('div');
             const removeButton = document.createElement('div');
             removeButton.classList.add('delete-button');
-            const removeIcon = document.createTextNode('X');
+            const removeIcon = document.createTextNode('-');
             li.classList.add('entry');
 
             removeButton.addEventListener('click', ()=>{
@@ -103,16 +140,16 @@ class Todo{
             });
 
             if(entryValue.done){
-                div.classList.add('task-completed');
+                li.classList.add('task-completed');
             }
 
-            div.addEventListener('click', (event)=>{
+            li.addEventListener('click', (event)=>{
                 event.target.classList.add('task-completed');
                 this.entries[entryIndex].done = true;
                 this.saveLocal();
             });
 
-            div.innerHTML = `${entryValue.title} <br> ${entryValue.description} <br> ${entryValue.date}`;
+            div.innerHTML = `<a> <b> ${entryValue.title} </b> <br> ${entryValue.description} <br> ${entryValue.date} </a>`;
 
             removeButton.appendChild(removeIcon);
             li.appendChild(div);
