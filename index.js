8a72549d58631490c3f0dbf5562fecb01516a5a7
Jan Morten Kivi <55129090+janmortenk@users.noreply.github.com>
2020-04-05 09:40:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-sdas
+class Entry{
+    constructor(title, description, date){
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.done = false;
+    }
+}
+
+class Todo{
+    constructor();
+    this.entries = JSON.parse(window.localStorage.getItem('entries')) || [];
+    document.querySelector('#addButton').addEventListener('click', ()=>{this.addEntry()});
+    this.render();
+}
+
+    addEntry(){
+        const titleValue = document.querySelector('#title').value;
+        const descriptionValue = document.querySelector('#description').value;
+        const dateValue = document.querySelector('#date').value;
+        this.entries.push(new Entry(titleValue, descriptionValue, dateValue));
+        this.saveOnLocalStorage();
+        this.render();
+    }
+ 
