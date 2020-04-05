ac31a90af98e996762d8b2a84f4f983f743d399e
Jan Morten Kivi <55129090+janmortenk@users.noreply.github.com>
2020-04-05 19:59:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,4 +22,56 @@ class Todo{
         this.saveOnLocalStorage();
         this.render();
     }
+    render(){
+        if(document.querySelector('.todo-list')){
+          document.body.removeChild(document.querySelector('.todo-list'));
+        }
+    
+        const ul = document.createElement("ul");
+        ul.setAttribute("id", "myUl");
+        ul.className = "todo-list";
+        this.entries.forEach((entry, entryIndex)=>{
+          const li = document.createElement("li");
+          li.innerHTML = `<b>${entry.title}</b> <br>  ${entry.description} <br>  ${entry.date}`;
+    
+          let span = document.createElement("SPAN");
+          let txt = document.createTextNode("\u00D7");
+          span.className = "close";
+          span.addEventListener("click", ()=>{
+            ul.removeChild(li);
+            this.entries = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex + 1, this.entries.length));
+            this.saveLocal();
+          });
+    
+          li.addEventListener('click', (event)=> {
+              event.target.classList.toggle('checked');
+              if(entry.done){
+                entry.done = false;
+              }
+              else{
+                entry.done = true;
+              }
+    
+              this.saveLocal();
+              this.render();
+          });
+    
+          if(entry.done){
+            li.classList.toggle('checked');
+          }
+    
+          span.appendChild(txt);
+          li.appendChild(span);
+          ul.appendChild(li);
+        });
+        document.body.appendChild(ul);
+      }
+    
+    
+      
+    
+      saveLocal(){
+        window.localStorage.setItem('entries', JSON.stringify(this.entries));
+      }
+    }
  
